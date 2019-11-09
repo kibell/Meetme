8cc4e787c34d247e700d093d3fff86e3fa8b84c3
Kitrick Bell <kitrickd.bell@gmail.com>
2019-11-09 08:33:17 -0600

    linking js file to the html files

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-AIzaSyDnVRNH-ywCoFfxB09yYaoLKb4ResRB7Ao
+console.log ("im working")
+
+var map;
+                      
+                      
+function initMap() {
+ map = new google.maps.Map(document.getElementById('map'), {
+center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+                      });
+                    }
