d297c66e96f5e5fc0a8c527f44820dc51c3a32b8
Kitrick Bell <kitrickd.bell@gmail.com>
2019-11-09 13:49:28 -0600

    added geolocation and geocoder to the app.js file. Console logged the lat and lng numbers we need for the next steps

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 console.log ("im working")
+const GeoLocationURL = "https://maps.googleapis.com/maps/api/geocode/json?address=77064&key=AIzaSyCM2ChWHyTWpo5OJQFdiI-4tLDFUugVc7Q"
+let map;
 
-var map;
-                      
+
+
 function initMap() {
-    var directionsRenderer = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
+    let directionsRenderer = new google.maps.DirectionsRenderer;
+    
+    let directionsService = new google.maps.DirectionsService;
+    let geocoder = new google.maps.Geocoder();
+    
+    
+ 
+    let map = new google.maps.Map(document.getElementById('map'), {
       zoom: 7,
       center: {lat: 41.85, lng: -87.65}
     });
     directionsRenderer.setMap(map);
     directionsRenderer.setPanel(document.getElementById('right-panel'));
-
-    var control = document.getElementById('floating-panel');
+    
+    let control = document.getElementById('floating-panel');
     control.style.display = 'block';
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-
-    var onChangeHandler = function() {
+    
+    let onChangeHandler = function() {
       calculateAndDisplayRoute(directionsService, directionsRenderer);
+      
     };
     document.getElementById('start').addEventListener('change', onChangeHandler);
     document.getElementById('end').addEventListener('change', onChangeHandler);
-  }
-
+  
   function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-    var start = document.getElementById('start').value;
-    var end = document.getElementById('end').value;
+    let start = document.getElementById('start').value;
+    let end = document.getElementById('end').value;
+    
     directionsService.route({
       origin: start,
       destination: end,
@@ -33,10 +41,73 @@ function initMap() {
     }, function(response, status) {
       if (status === 'OK') {
         directionsRenderer.setDirections(response);
+        console.log(response)
       } else {
         window.alert('Directions request failed due to ' + status);
       }
     });
   }
+
+
+
+
+$( document).ready(function(){
+document.getElementById('findLocation').addEventListener('click', function(){
+console.log("hey");
+geocodeAddress(geocoder, map);
+function geocodeAddress(geocoder, resultsMap) {
+    // let address = document.getElementById('inputTest').value;
+    let address = "Sydney, NSW";
+    console.log(address);
+    geocoder.geocode({'address': address}, function(results, status) {
+        console.log(results);
+              if (status === 'OK') {
+                console.log(results[0].geometry.location.lat());
+                console.log(results[0].geometry.location.lng());
+                let marker = new google.maps.Marker({
+                  map: resultsMap,
+                  position: results[0].geometry.location
+                });
+              } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+              }
+            });
+          
+
+
+
+}
+
+
+
+
+
+
+
+});
+
+});
+}
+
+//   $('#findLocation').click(function() {
+//       console.log("hey")
+//   function geocodeAddress(geocoder, resultsMap) {
+//     let address = document.getElementById('start').value;
+//     geocoder.geocode({'start': address}, function(results, status) {
+//       if (status === 'OK') {
+//         console.log(results[0].geometry.location);
+//         let marker = new google.maps.Marker({
+//           map: resultsMap,
+//           position: results[0].geometry.location
+//         });
+//       } else {
+//         alert('Geocode was not successful for the following reason: ' + status);
+//       }
+//     });
+//   }
+
+//   });
+
+
 
   
