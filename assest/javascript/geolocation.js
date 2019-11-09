bdb0ca38b6a930849d6db2ccb3bb695bbf1f4efa
Dipti Katkar <dk.aj1990@gmail.com>
2019-11-09 10:37:27 -0600

    Added directions on geolocation

--- assest/javascript/geolocation.js
+++ assest/javascript/geolocation.js
@@ -12,3 +12,49 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 }
+
+//map page
+let y = document.getElementById("map-canvas");
+
+let mapLatitude;
+let mapLongitude;
+let myLatLang;
+
+function getMapLocation(){
+    console.log("getMapLocation");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showMapPosition);
+    } else {
+        y.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showMapPosition(position) {
+	console.log("showMapPosition");
+    mapLatitude = position.coords.latitude;
+    mapLongitude = position.coords.longitude;
+    myLatlng = new google.maps.LatLng(mapLatitude,mapLongitude);
+    getMap();
+    
+}
+
+var map;
+function getMap() {
+	console.log("getMap");
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(mapLatitude, mapLongitude)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+	var marker = new google.maps.Marker({
+	    position: myLatlng,
+	    map: map,
+	    title:"You are here!"
+	});
+}
+
+$( document ).on( "pageshow", "#mapPage", function( event ) {
+  getMapLocation();
+});
